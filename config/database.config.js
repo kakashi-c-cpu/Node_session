47ec470e72162482e4b111da8f6dc97a9c5ad6e0
kakashi-c-cpu <nhatst543@gmail.com>
2024-12-17 16:41:19 +0700

    Update database.config.js

--- config/database.config.js
+++ config/database.config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://nhatst543:<nhat1332005>@cluster0.35poo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+        await mongoose.connect('mongodb+srv://nhatst543:nhat1332005@cluster0.35poo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
         console.log('Kết nối MongoDB thành công');
     } catch (err) {
         console.error('Lỗi kết nối MongoDB:', err);
